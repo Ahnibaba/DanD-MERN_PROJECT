6562bf20da627474fcf570736fcece010129adbf
Ahnibaba <aniebietedet2025@gmail.com>
2024-06-26 19:26:16 +0100

    deploy

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getAllUsers, createNewUser, updateUser, deleteUser } = require("../controllers/usersController");
+const verifyJWT = require("../middleware/verifyJWT");
 
+router.use(verifyJWT)
 
 router.route("/")
    .get(getAllUsers)
