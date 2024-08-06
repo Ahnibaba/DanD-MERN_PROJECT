14677cf3b1f83fc646abece40ade9c3b1f7b20d9
Ahnibaba <aniebietedet2025@gmail.com>
2024-08-06 20:52:10 +0100

    adjust

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials } from '../../features/auth/authSlice'
 
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:3500",
+    baseUrl: "https://technotesserver.onrender.com",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
